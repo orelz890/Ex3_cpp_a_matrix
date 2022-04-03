1f3eb1cc52ddf3f56ef583f37aad51ad7fe72c7e
orel <orelzx13@gmail.com>
2022-04-03 23:03:01 +0300

    more tests

--- Test.cpp
+++ Test.cpp
@@ -10,15 +10,18 @@ std::vector<double> identity = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 std::vector<double> negIdentity = {-1, 0, 0, 0, -1, 0, 0, 0, -1};
 std::vector<double> arr3 = {3, 0, 0, 0, 3, 0, 0, 0, 3};
 std::vector<double> arr4 = {3, 1, 1, 3, 4, 0, 0, 1, 3, 2, 2, 0, 0, 0, 0, 0};
+std::vector<double> mat_data = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+std::vector<double> mat_data2 = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+std::vector<double> fourX3 = {3, 2, 1, 3, 2, 1, 3, 2, 1, 3, 2, 1};
 
 const Matrix matId{identity, 3, 3};
 const Matrix matNegId{negIdentity, 3, 3};
 Matrix mat3on3{arr3, 3, 3};
 Matrix mat4on4{arr4, 4, 4};
+Matrix mat4on3{fourX3, 4, 3};
+
 
 TEST_CASE("Good tests"){
-    std::vector<double> mat_data = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-    std::vector<double> mat_data2 = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
     SUBCASE("Basic math test"){
         Matrix mat{mat_data, 3, 3};
@@ -29,7 +32,7 @@ TEST_CASE("Good tests"){
         for (size_t i = 2; i < 10; i++){
             CHECK(--mat2 == mat);
             CHECK(++mat2 == mat*2);
-            // Checking that the position of the operator metters
+            // Checking that the position of the operator matters
             CHECK(mat2++ == (mat+mat));
             CHECK(mat2-- == (mat+mat));
             CHECK(++mat3 == mat * i);
@@ -60,6 +63,13 @@ TEST_CASE("Good tests"){
                 mat -= temp2;
                 CHECK(temp == mat);
             }
+            // Checking that * and *= returns the same matrix
+            for (int i = -10; i < 10; i++){
+                double scalar = (double)i;
+                Matrix temp = mat * scalar;   
+                mat *= temp;
+                CHECK(temp == (mat * temp));
+            }
         }
         SUBCASE("Matrix & scalar"){
 
@@ -70,11 +80,12 @@ TEST_CASE("Good tests"){
                 mat *= scalar;
                 CHECK(temp == mat);
             }
+            CHECK_FALSE((mat * 2) == mat);
         }
 
     }
 
-    SUBCASE("Matrix compare"){
+    SUBCASE("Matrix comparing tests"){
 
         Matrix mat{mat_data, 3, 3};
         Matrix mat2{mat_data2, 3, 3};
@@ -106,7 +117,7 @@ TEST_CASE("Good tests"){
         }
     }
 
-    SUBCASE(""){
+    SUBCASE("Input/Output operations"){
 
     }
 
@@ -114,8 +125,43 @@ TEST_CASE("Good tests"){
 
 
 TEST_CASE("Bad input") {
+    Matrix mat{mat_data, 3, 3};
+    Matrix mat2{mat_data2, 3, 3};
+    Matrix mat3{mat_data, 3, 3};
+    std::vector<double> empty_data = {};
 
-    CHECK_THROWS();
+
+
+    SUBCASE("Negetive input"){
+        
+        for (int i = -50; i < 0; i++){
+            CHECK_THROWS((Matrix{mat_data, i, 3}));
+            CHECK_THROWS((Matrix{mat_data, 3, i}));
+            CHECK_THROWS((Matrix{mat_data, i, i}));
+        }
+    }
+
+    SUBCASE("Illigal empty case"){
+        CHECK_THROWS((Matrix{mat_data, 0, 3}));
+        CHECK_THROWS((Matrix{mat_data, 3, 0}));  
+        CHECK_THROWS((Matrix{mat_data, 0, 0}));
+        CHECK_THROWS((Matrix{empty_data, 3, 3}));
+    }
+
+    SUBCASE("Matrix size not equels Row*Col == size"){
+        for (int i = 0; i < 50; i++){
+            CHECK_THROWS((Matrix{fourX3, i, i}));
+            CHECK_THROWS((Matrix{fourX3, i, i + 2}));
+            CHECK_THROWS((Matrix{fourX3, i, i + 3}));
+        }
+        
+    }
+
+}
+
+TEST_CASE("Illigal operations"){
+
+    
 
 
 

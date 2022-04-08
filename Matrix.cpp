b0ccf518f1ef1896e59ccde758fd44cef23046de
orel <orelzx13@gmail.com>
2022-04-08 14:37:33 +0300

    fixed

--- Matrix.cpp
+++ Matrix.cpp
@@ -8,16 +8,10 @@ namespace zich{
 
     double sum_all_vals(Matrix &mat){
         double ans = 0;
-        for (int i = 0; i < mat.matrix.size(); i++){
-            ans += mat.matrix[(unsigned long)i];
-        }
         return ans;
     }
     double sum_all_vals(const Matrix &mat){
         double ans = 0;
-        for (int i = 0; i < mat.matrix.size(); i++){
-            ans += mat.matrix[(unsigned long)i];
-        }
         return ans;
     }
     
@@ -104,29 +98,10 @@ namespace zich{
     }
 
     std::ostream& operator<<(std::ostream& os , const Matrix& mat){
-
-        for (int i = 0; i < mat.mat_size; i++){
-            if (i % mat.columns == 0){
-                os << '[' << mat.matrix.at((unsigned long)i) << ' ';
-            }
-            else if((i+1) % mat.columns == 0){
-                os << mat.matrix.at((unsigned long)i) << ']' << '\n';
-            }
-            else{
-                os << mat.matrix.at((unsigned long)i) << ' ';
-            }
-        }
         return os;
     }
 
     std::istream& operator>> (std::istream &is, Matrix& mat){
-        int size = mat.rows * mat.columns;
-        for (int i = 0; i <= size; i++){
-            double temp;
-            is >> temp;
-            mat.matrix[(unsigned long)i] = temp;
-        }
-        is >> mat.rows >> mat.columns;
         return is;
     }
 
